1032a84038b85ba71cb52ab44b18cab45c52fe6a
zxnote <112752263+zxnote@users.noreply.github.com>
2025-01-14 16:57:07 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'StevenTzy'// yourname
+global.MONGO_DB_URI = "mongodb+srv://steven:bagas0101@cluster0.3sztz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "agenalinl@gmail.com" //email
+global.email_password = "muoi nugo bxap ukpu" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
